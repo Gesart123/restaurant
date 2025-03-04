6cb049568f5a3feb678b2473a6a87bed91a3f49f
Gesart Mlloja <gesmll20@htl-shkoder.com>
2025-03-04 12:08:47 +0100

    new

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	let {data} = $props();
 </script>
-
+<div>
 <h1>Restaurants in Shiroka</h1>
 <p>
 	All API endpoints are defined in the <code>src/routes/api</code> folder. Start with the
@@ -15,11 +15,54 @@
 	{/each}
 </ul>
 
+</div>
 
 <style>
-	 
-	h1{
-		
+div {
+		font-family: Arial, sans-serif;
+		background-color: #F8F9FA;
+		color: #333;
+		margin: 0;
+		padding: 20px;
+		text-align: center;
 	}
 
+	
+	h1 {
+		background-color: #2C3E50;
+		color: #ECF0F1;
+		padding: 20px;
+		margin: 0;
+		border-radius: 5px;
+	}
+	
+	
+	p {
+		color: #555;
+		margin: 15px 0;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0;
+		margin-top: 20px;
+	}
+
+	li {
+		margin: 10px 0;
+	}
+
+	a {
+		color: #3498DB;
+		text-decoration: none;
+		font-weight: bold;
+		transition: color 0.3s ease;
+	}
+
+	a:hover {
+		color: #1ABC9C;
+	}
 </style>
+
+
+
